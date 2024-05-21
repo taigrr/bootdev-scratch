f8e9316d1a6a84b3faa5afb3427bc851e4d74166
Tai Groot <tai@taigrr.com>
2024-05-21 13:27:54 -0700

    finish course bookbot

--- main.py
+++ main.py
@@ -1,55 +1,21 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
+#!/usr/bin/env python3
+with open("books/frankenstein.txt") as file:
+    text = file.read()
+    wc = len(text.split())
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{wc} words found in the document")
     print()
-
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"The '{item['char']}' character was found {item['num']} times")
-
-    print("--- End report ---")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-
-def sort_on(d):
-    return d["num"]
-
-
-def chars_dict_to_sorted_list(num_chars_dict):
-    sorted_list = []
-    for ch in num_chars_dict:
-        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
-    sorted_list.sort(reverse=True, key=sort_on)
-    return sorted_list
-
-
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
+    charDict = {}
+    for char in text:
+        char = char.lower()
+        if char in charDict:
+            charDict[char] += 1
         else:
-            chars[lowered] = 1
-    return chars
-
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-
-main()
+            charDict[char] = 1
+    ldict = sorted(charDict.items(), key=lambda x: x[1], reverse=True)
+    for x in ldict:
+        if not x[0].isalpha():
+            continue
+        print(f"The '{x[0]}' character was found {x[1]} times")
+    print("--- End report ---")
 
