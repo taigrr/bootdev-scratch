f1ec9536d4c41efa8e98883b82bcf0c947176de1
Tai Groot <tai@taigrr.com>
2024-05-21 17:41:57 -0700

    starting on ssg

---
+++ ssg/src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from textnode import TextNode
+def main():
+    node = TextNode("Hello, World!", "bold", "taigrr.com")
+    print(node)
+
+if __name__ == "__main__":
+    main()
